8b64233550417da4372c05830399257b9ee139ef
vxplore <vik@v-xplore.com>
2022-04-09 18:44:37 +0530

    1

--- jusi.js
+++ jusi.js
@@ -1,3 +1,19 @@
 class Widget {
-    
+    constructor(){
+        this.children = []
+    }
+    add(widget){
+        if(widget instanceof Widget){
+            this.children.push(widget)
+        }
+    }
+}
+
+class Div extends Widget {
+    constructor(){
+        this.children = []
+    }
+    build(){
+
+    }
 }
